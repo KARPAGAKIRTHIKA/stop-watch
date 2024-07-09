0a34aba3aeace3d419a9bf4511d71fc9fb0e6297
karpagakirthika <velusamyg2004@gmail.com>
2024-07-09 18:23:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import StopWatchComponent from './assets/Components/StopWatchComponent/StopWatchComponent'
+function App() {
+
+  return (
+    <StopWatchComponent/>
+  )
+}
+
+export default App
